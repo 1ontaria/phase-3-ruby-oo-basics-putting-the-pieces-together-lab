899957a7de2c23192e3b84d0ecc526a4ba0a31d9
Ontaria Woods <ontaria.woods@gmail.com>
2023-06-29 06:15:43 -0400

    commit tree a738274e313cd13d5d7797e8fa4ea4b23a677c83

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+require 'pry'
+
+class Book
+    attr_reader :title, :turn_page
+    attr_accessor :author, :page_count, :genre
+
+def initialize(title)
+    @title = title
+   
+end
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+def author_name author
+    @author = author
+end
+
+
+end
+
+puts coldest = Book.new("Coldest Winter Ever")
+coldest.turn_page
+
 
 
